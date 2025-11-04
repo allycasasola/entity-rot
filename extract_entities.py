515f2c3e35497557a6d5837e7c886d77c4d93ffb
allyyc <allisonscasasola@gmail.com>
2025-11-04 00:13:29 -0800

    Adding date

--- extract_entities.py
+++ extract_entities.py
@@ -14,6 +14,7 @@ import google.generativeai as genai
 from pydantic import BaseModel
 from tqdm import tqdm
 from dotenv import load_dotenv
+from datetime import datetime
 
 EntityType = Literal["organization", "location", "role", "event", "other"]
 
